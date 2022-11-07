f287da530f40ddd9f066e088b007fe7a53330213
albadev <joseadauto923@gmail.com>
2022-11-06 21:16:52 -0300

    User Control

---
+++ Controllers/UserControlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtlasControl.Controllers
+{
+    public class UserControlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

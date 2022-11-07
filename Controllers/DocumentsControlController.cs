4b120a3747a6ae3aebc6b72aaae02a1a296bb175
albadev <joseadauto923@gmail.com>
2022-11-06 22:44:04 -0300

    Back button in all pages | Documents Controller

---
+++ Controllers/DocumentsControlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtlasControl.Controllers
+{
+    public class DocumentsControlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
